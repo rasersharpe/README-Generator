17044db2e95e42dc43609ee528f2edf51642be82
rasersharpe <jay.bhatt@me.com>
2025-01-21 16:44:43 -0600

    Made License functions and generateMarkdown function.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# README-Generator
-A professional README generator.
+# ReadME
+  ![License](https://img.shields.io/badge/License-GPL-green)
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [License](#license)
+
+## Description
+
+BLAH
+
+## Installation
+
+BHAL
+
+## License
+    This project is licensed with GPL
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,55 @@
 // TODO: Create a function that returns a license
 // badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // https://img.shields.io/badge/something-anything-green
+  if (license === "None") {
+    return "";
+  } else {
+    return `![License](https://img.shields.io/badge/License-${license}-green)`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return "- [License](#license)";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return `## License
+This project is licensed with ${license}`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
+  ${renderLicenseBadge(data.projectLicense)}
+## Table of Contents
 
+- [Description](#description)
+- [Installation](#installation)
+${renderLicenseLink(data.projectLicense)}
+
+## Description
+
+${data.projectDescription}
+
+## Installation
+
+${data.projectInstallation}
+
+${renderLicenseSection(data.projectLicense)}
 `;
 }
 
